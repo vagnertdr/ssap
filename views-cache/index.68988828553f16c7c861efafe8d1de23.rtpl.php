c0d38fec5f50e68c35bebbe4cfafebc0302a7611
vagnertdr <vagnerrochab2@gmail.com>
2020-08-22 21:21:10 -0300

    Adicionando e configurando template do Admin

--- views-cache/index.68988828553f16c7c861efafe8d1de23.rtpl.php
+++ views-cache/index.68988828553f16c7c861efafe8d1de23.rtpl.php
@@ -1,1 +1,202 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Hello SSA!</h1>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="slider-area">
+    <!-- Slider -->
+    <div class="block-slider block-slider4">
+        <ul class="" id="bxslider-home4">
+            <li>
+                <img src="/res/site/img/h4-slideR.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">
+                        Kit <span class="primary"> <strong>Festas</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">Dual SIM</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+            <li><img src="/res/site/img/h4-slide2R.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">Bolos, Tortas <span class="primary"> e <strong>muito mais</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">Acesse para mais opções</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+            <li><img src="/res/site/img/h4-slide3R.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">
+                        Salgados Fritos <span class="primary">de Variados  <strong>Tipos</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">Acesse para mais opções</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+            <li><img src="/res/site/img/h4-slide4R.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">
+                        Temos também <span class="primary">opções de <strong>Assados</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">Acesse para mais opções</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <!-- ./Slider -->
+</div> <!-- End slider area -->
+
+<div class="promo-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo promo1">
+                    <i class="fa fa-refresh"></i>
+                    <p>Irá ser removido</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo promo2">
+                    <i class="fa fa-truck"></i>
+                    <p>Frete grátis</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo promo3">
+                    <i class="fa fa-lock"></i>
+                    <p>Pagamento seguro</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo promo4">
+                    <i class="fa fa-gift"></i>
+                    <p>Novos produtos</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End promo area -->
+
+<div class="maincontent-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="latest-product">
+                    <h2 class="section-title">Produtos</h2>
+                    <div class="product-carousel">
+                        <div class="single-product">
+                            <div class="product-f-image">
+                                <img src="/res/site/img/product-1.jpg" alt="">
+                                <div class="product-hover">
+                                    <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                    <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                </div>
+                            </div>
+
+                            <h2><a href="#">Samsung Galaxy s5- 2015</a></h2>
+
+                            <div class="product-carousel-price">
+                                <ins>$700.00</ins> <del>$100.00</del>
+                            </div>
+                        </div>
+                        <div class="single-product">
+                            <div class="product-f-image">
+                                <img src="/res/site/img/product-2.jpg" alt="">
+                                <div class="product-hover">
+                                    <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                    <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                </div>
+                            </div>
+
+                            <h2>Nokia Lumia 1320</h2>
+                            <div class="product-carousel-price">
+                                <ins>$899.00</ins> <del>$999.00</del>
+                            </div>
+                        </div>
+                        <div class="single-product">
+                            <div class="product-f-image">
+                                <img src="/res/site/img/product-3.jpg" alt="">
+                                <div class="product-hover">
+                                    <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                    <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                </div>
+                            </div>
+
+                            <h2>LG Leon 2015</h2>
+
+                            <div class="product-carousel-price">
+                                <ins>$400.00</ins> <del>$425.00</del>
+                            </div>
+                        </div>
+                        <div class="single-product">
+                            <div class="product-f-image">
+                                <img src="/res/site/img/product-4.jpg" alt="">
+                                <div class="product-hover">
+                                    <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                    <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                </div>
+                            </div>
+
+                            <h2><a href="#">Sony microsoft</a></h2>
+
+                            <div class="product-carousel-price">
+                                <ins>$200.00</ins> <del>$225.00</del>
+                            </div>
+                        </div>
+                        <div class="single-product">
+                            <div class="product-f-image">
+                                <img src="/res/site/img/product-5.jpg" alt="">
+                                <div class="product-hover">
+                                    <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                    <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                </div>
+                            </div>
+
+                            <h2>iPhone 6</h2>
+
+                            <div class="product-carousel-price">
+                                <ins>$1200.00</ins> <del>$1355.00</del>
+                            </div>
+                        </div>
+                        <div class="single-product">
+                            <div class="product-f-image">
+                                <img src="/res/site/img/product-6.jpg" alt="">
+                                <div class="product-hover">
+                                    <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                    <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                </div>
+                            </div>
+
+                            <h2><a href="#">Samsung gallaxy note 4</a></h2>
+
+                            <div class="product-carousel-price">
+                                <ins>$400.00</ins>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End main content area -->
+
+<div class="brands-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="brand-wrapper">
+                    <div class="brand-list">
+                        <img src="/res/site/img/brand1.png" alt="">
+                        <img src="/res/site/img/brand2.png" alt="">
+                        <img src="/res/site/img/brand3.png" alt="">
+                        <img src="/res/site/img/brand4.png" alt="">
+                        <img src="/res/site/img/brand5.png" alt="">
+                        <img src="/res/site/img/brand6.png" alt="">
+                        <img src="/res/site/img/brand1.png" alt="">
+                        <img src="/res/site/img/brand2.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End brands area -->
